bcb7afb935fdbf07d9557c094a52ed0e79059e5e
Michael J. Schiumo <mschiumo18@gmail.com>
2019-02-05 15:52:09 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,10 +1,12 @@
 require 'open-uri'
 require 'pry'
+require 'nokogiri'
 
 class Scraper
 
   def self.scrape_index_page(index_url)
-    
+    html = http://159.89.235.135:47318
+    doc = Nokogiri::HTML(html)
   end
 
   def self.scrape_profile_page(profile_url)
