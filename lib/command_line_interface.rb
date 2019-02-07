8bb1202d9641352340ab1280b30bdae8c24705bb
Ethan Rosenberg <ethan@ethanrosenberg.com>
2019-02-07 23:49:06 +0000

    Done.

--- lib/command_line_interface.rb
+++ lib/command_line_interface.rb
@@ -2,6 +2,7 @@ require_relative "../lib/scraper.rb"
 require_relative "../lib/student.rb"
 require 'nokogiri'
 require 'colorize'
+require'pry'
 
 class CommandLineInterface
   BASE_PATH = "./fixtures/student-site/"
@@ -19,6 +20,7 @@ class CommandLineInterface
 
   def add_attributes_to_students
     Student.all.each do |student|
+      #binding.pry
       attributes = Scraper.scrape_profile_page(BASE_PATH + student.profile_url)
       student.add_student_attributes(attributes)
     end
--- lib/scraper.rb
+++ lib/scraper.rb
@@ -3,12 +3,43 @@ require 'pry'
 
 class Scraper
 
-  def self.scrape_index_page(index_url)
-    
+     def self.scrape_index_page(index_url)
+   index_data = []
+    doc = Nokogiri::HTML(open(index_url))
+     students = doc.css("div .student-card")
+
+     students.each do |student|
+     
+       name = student.css("a .card-text-container .student-name").text
+       location = student.css("a .card-text-container .student-location").text
+       profile_url = student.css("a").attribute("href").text
+       index_data << {:name => name, :location => location, :profile_url => profile_url}
+       #index_data << {:name => name, {:location => location, :profile_url => profile_url}}
+     end
+     index_data
   end
 
-  def self.scrape_profile_page(profile_url)
-    
+ def self.scrape_profile_page(profile_url)
+    profile_hash = {}
+    doc = Nokogiri::HTML(open(profile_url))
+    social = doc.css(".social-icon-container a")
+
+    social.each do |social_link|
+      if social_link.attribute("href").text.include?("twitter.com")
+           profile_hash[:twitter] = social_link.attribute("href").text
+      elsif social_link.attribute("href").text.include?("github.com")
+        profile_hash[:github] = social_link.attribute("href").text
+      elsif social_link.attribute("href").text.include?("linkedin.com")
+        profile_hash[:linkedin] = social_link.attribute("href").text
+      else
+        profile_hash[:blog] = social_link.attribute("href").text
+      end
+
+    end
+    profile_hash[:profile_quote] = doc.css(".profile-quote").text.strip
+    profile_hash[:bio]= doc.css(".bio-content .description-holder").text.strip
+
+     profile_hash
   end
 
 end
